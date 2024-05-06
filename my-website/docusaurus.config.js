620f8a5d1d1929749362cc1d09e33642c30ac37c
FalecidoGugu <gutigeovany@gmail.com>
2024-05-05 23:30:14 -0300

    Adicionado dropdowns na navbar

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -4,7 +4,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Documentation",
-  tagline: "Vá para documentos",
+  tagline: "Por favor, ative o modo escuro.",
   favicon: "img/favicon.ico",
 
   trailingSlash: false,
@@ -64,12 +64,55 @@ const config = {
           label: "Home",
         },
         {
-          to: "/Documentation/docs/category/anotações-do-javascript",
-          label: "JavaScript",
-          position: "left",
+          label: 'Fundamentos', // Este é o texto que aparecerá na navbar
+          position: 'left',
+          items: [ // Os itens do dropdown de primeiro nível
+            {
+              to: '/Documentation/docs/Home/Fundamentos/intro',
+              label: 'Fundamentos Importantes do C#', // O texto que aparecerá no dropdown
+            },
+            {
+              to: '/Documentation/docs/Home/Fundamentos/mobileesqlite/conceitosMobile',
+              label: 'Conceitos em Mobile: Conceitos Importantes',
+            },
+            {
+              to: '/Documentation/docs/Home/Fundamentos/mobileesqlite/sqlite',
+              label: 'Conceitos em Mobile: SQLite',
+            },
+            {
+              to: '/Documentation/docs/Home/Fundamentos/pooesolid/poo',
+              label: 'POO e Princípios SOLID: Princípios',
+            },
+            {
+              to: '/Documentation/docs/Home/Fundamentos/pooesolid/solid',
+              label: 'POO e Princípios SOLID: Princípios SOLID',
+            },
+            // Adicione mais itens aqui se necessário
+          ],
         },
+        
+        {
+          label: 'Desenvolvimento Web', // Este é o texto sobre o qual o usuário passará o mouse
+          position: 'left',
+          items: [ // Itens do dropdown
+            {
+              to: '/Documentation/docs/category/seção-html', // O caminho para a página de HTML
+              label: 'HTML', // O texto que aparecerá no dropdown
+            },
+            /*{
+              to: '/docs/web-development/css',
+              label: 'CSS',
+            },*/
+            {
+              to: "/Documentation/docs/category/anotações-do-javascript",
+              label: "JavaScript",
+            },
+            // Você pode adicionar mais itens aqui
+          ],
+        },    
       ],
     },
+
     footer: {
       style: "dark",
       links: [
